d5ffa7d6bda3a1b922af4bfdee95b8f919255afb
iibrahimli <imranibrahimli98@gmail.com>
2019-09-30 15:10:32 +0400

    add opencv webcam stream

--- src/ydl_test.cpp
+++ src/ydl_test.cpp
@@ -1,10 +1,43 @@
 #include <iostream>
 
 #include "darknet.h"
+#include "opencv2/opencv.hpp"
 
-int main(){
+using namespace cv;
 
-    std::cout << "OK" << std::endl;
+
+
+void parse_cfg(char *datacfg, char *cfgfile, char *weightfile){
+
+    
+
+}
+
+
+
+int main() {
+
+    VideoCapture cap(0);    // open the default camera
+    if(!cap.isOpened())     // check if we succeeded
+        return -1;
+
+    std::cout << "Camera OK" << std::endl;
+
+    namedWindow("capture");
+
+    for(;;) {
+        Mat frame;
+        cap >> frame;
+
+        if(frame.empty()) continue;
+
+        imshow("capture", frame);
+
+        if(waitKey(1) != 255) break;    // stop capturing by pressing ESC
+    }
+
+    cap.release();
+    destroyAllWindows();
 
     return 0;
 }
