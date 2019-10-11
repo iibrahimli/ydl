b579389ba3ae3bd3702dd3fe67ba963d982447d9
iibrahimli <imranibrahimli98@gmail.com>
2019-10-11 13:40:45 +0400

    add driver code

--- src/ydl_test.cpp
+++ src/ydl_test.cpp
@@ -1,29 +1,41 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include "ydl/detector.hpp"
 
-#include "darknet.h"
-#include "opencv2/opencv.hpp"
-
+using std::cout;
+using std::endl;
 using namespace cv;
 
 
-
-void parse_cfg(char *datacfg, char *cfgfile, char *weightfile){
-
-    
-
-}
+#define CAP_WIDTH  1280
+#define CAP_HEIGHT 720
 
 
 
 int main() {
 
-    VideoCapture cap(0);    // open the default camera
-    if(!cap.isOpened())     // check if we succeeded
-        return -1;
+    std::string cfg_filename     = "../model/yolov3_tiny_test.cfg";
+    std::string weights_filename = "../model/yolov3_tiny_10k.weights";
+    std::string names_filename   = "../model/objects.names";
 
-    std::cout << "Camera OK" << std::endl;
+    ydl::detector det(cfg_filename, weights_filename, names_filename);
+
+
+    VideoCapture cap(0);     // open the default camera
+    if(!cap.isOpened()){     // check if we succeeded
+        cout << "Unable to open camera" << endl;
+        return -1;
+    }
+
+    cout << "Camera OK" << std::endl;
+
+    cap.set(CV_CAP_PROP_FRAME_WIDTH,  CAP_WIDTH);
+    cap.set(CV_CAP_PROP_FRAME_HEIGHT, CAP_HEIGHT);
 
     namedWindow("capture");
+
+    // ydl::v_pred_result res;
+    // ydl::duration dur;
 
     for(;;) {
         Mat frame;
@@ -31,9 +43,14 @@ int main() {
 
         if(frame.empty()) continue;
 
+        // predict and annotate
+        auto [res, dur] = det.predict(frame, 0.5);
+        cout << res << endl;
+        frame = det.annotate(frame, res, dur);
+
         imshow("capture", frame);
 
-        if(waitKey(1) != 255) break;    // stop capturing by pressing ESC
+        if(waitKey(1) != 255) break;
     }
 
     cap.release();
